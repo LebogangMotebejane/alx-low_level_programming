6d0bdfbd994e797670daf33fc87c9659128be0d8
LebogangMotebejane <lebogangmotes@gmail.com>
2023-02-19 13:44:45 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - print out sizes of data types in C
- * Return: Always 0 (success)
+* main - print out sizes of data types in C
+* Return: Always 0 (success)
 */
 
 int main(void)
